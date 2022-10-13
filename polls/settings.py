4bee674cfa889c788edd5ee41ebe1fbc470d1a10
Moh Ariful <nextkoding@gmail.com>
2022-10-13 12:25:27 +0700

    Ready to upload to github

--- polls/settings.py
+++ polls/settings.py
@@ -9,7 +9,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0e#=!q#x&gghh7kinvxm^zji1(p260qktw6i$&tu0m3z0to6w5'
+SECRET_KEY = 'django-insecure-xxxxxxxxxxxxxxxxxxxxxxxxx(xxxxxxxxxxxxxxxxxxxxxxxx'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
